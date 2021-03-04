ab6aeb733c473d030bd7b05be4ebe4f74aa4d288
fredyvalodie <fredyvalodie@gmail.com>
2021-03-04 21:03:30 +0000

    update_04/03/2021

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const title = document.getElementById("big_title");
 title.innerHTML = "Just Enjoy Food !";
+
+title.style.color = "blue";
+
+const home = document.getElementById("home_link");
+home.setAttribute("href", "https://www.google.com/");
